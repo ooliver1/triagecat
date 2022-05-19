e7b0b142024e5db7c3bf81f40f735d2cae39d3f2
ooliver1 <oliverwilkes2006@icloud.com>
2022-05-19 19:00:39 +0100

    fix: comment out unused handlers and add prep

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,5 +2,6 @@
 
 export { default as pullRequestHandler } from "./handlers.d/pull-request-handler";
 export { default as pullRequestReviewHandler } from "./handlers.d/pull-request-review-handler";
-export { default as issuesHandler } from "./handlers.d/issues-handler";
-export { default as workflowDispatchHandler } from "./handlers.d/workflow-dispatch-handler";
+// export { default as issuesHandler } from "./handlers.d/issues-handler";
+// export { default as workflowDispatchHandler } from "./handlers.d/workflow-dispatch-handler";
+// export { default as issueCommentHandler } from "./handlers.d/issue-comment-handler";
